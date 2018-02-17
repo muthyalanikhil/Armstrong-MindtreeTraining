358c8dca6eec2300c5f82f8b12588531c91a3687
nikhil muthyala <s528754@nwmissouri.edu>
2018-02-16 20:18:36 -0600

    Uploading to GitHub

---
+++ Armstrong/Armstrong.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Armstrong
+{
+    class Armstrong
+    {
+        public void FindingArmstrong()
+        {
+
+            int lengthOfnumber = 0, count = 0;
+            Console.WriteLine("\nenter a number: ");
+
+            string input = Console.ReadLine();
+
+            char[] separation = new char[] { ',' };
+            string[] separatedArray = input.Split(separation, StringSplitOptions.RemoveEmptyEntries);
+
+            for (int index = 0; index < separatedArray.Length; index++)
+            {
+                lengthOfnumber++;
+            }
+
+            int[] number = new int[100000];
+
+            for (int iteration = 0; iteration < lengthOfnumber; iteration++)
+            {
+                int sumOfDigits = 0;
+                int maximum = separatedArray[iteration].Length;
+                for (count = 0; count < maximum; count++)
+                {
+                    number[count] = separatedArray[iteration][count];
+                }
+                for (int index = 0; index < maximum; index++)
+                {
+                    int[] output = new int[maximum];
+                    int originalNumber = number[index] - 48;
+                    output[index] = (int)Math.Pow(originalNumber, maximum);
+                    sumOfDigits = sumOfDigits + output[index];
+                }
+                string compareNumber = Convert.ToString(sumOfDigits);
+                if (compareNumber == separatedArray[iteration])
+                {
+                    Console.Write("true ");
+                }
+                else
+                {
+                    Console.Write("False ");
+                }
+            }
+        }
+    }
+}
---
+++ Armstrong/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Armstrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                Armstrong number = new Armstrong();
+                number.FindingArmstrong();
+                Console.WriteLine("\nDo you want to continue? y/n");
+                string input = Console.ReadLine();
+                if (input == "y")
+                {
+                    number.FindingArmstrong();
+                }
+                else {
+                    break;
+                }
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                    Console.WriteLine("Enter Valid Inputs.");
+                }
+            }
+        }
+    }
+}
